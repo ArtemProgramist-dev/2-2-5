e1b117c77ca67d9b899aa29a280a7effe2d0a68a
ArtemProgramist-dev <supernikigma@gmail.com>
2026-01-02 14:20:26 +0600

    moder

---
+++ Модернизированный .py
@@ -0,0 +1,30 @@
+#простой калькулятор
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+num3 = float(input("Введите третье число: "))
+
+print("операции")
+print("1:сложение")
+print("2:вычитание")
+print("3:умножение")
+print("4:деление")
+
+operation = input("выберите операцию: ")
+
+if operation == "1":
+    result = num1 + num2 + num3
+    print(result)
+elif operation == "2":
+    result = num1 - num2 - num3
+    print(result)
+elif operation == "3":
+    result = num1 * num2 * num3
+    print(result)
+elif operation == "4":
+    result = num1 / num2 / num3
+    if num2 != 0:
+        print(result)
+    else: 
+        print("ZeroDivisionError")
+else:
+    print("неверные числа")
